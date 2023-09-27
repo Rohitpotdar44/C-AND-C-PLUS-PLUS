a5489df0625c5ac7d2651db7680c9000999648a8
Rohitpotdar44 <potdarrohit59@gmail.com>
2023-09-27 16:24:55 +0530

    C Programs

---
+++ C & C++ CODE/C_Programs/tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()                              /* escape sequesces= \n=newline & \t=space */
+{
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n"); //this is a print statement
+    printf("\" I am a boy\" - said one boy");
+    return 0;
+}

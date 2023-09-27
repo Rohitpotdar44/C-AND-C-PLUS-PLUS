8c80b1d32111a483f28a31ff05655ff2c865a698
Rohitpotdar44 <potdarrohit59@gmail.com>
2023-09-27 19:30:19 +0530

    C Programs

---
+++ C & C++ CODE/C_Programs/math_functions.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double a=sqrt(9);
+    double b=pow(2,4);
+    int c=ceil(3.59);
+    int d=floor(3.14);
+    int e =fabs(-341);
+    double f=round(4.578899);
+    double g=log(5);
+    double h=sin(45);
+    double i=cos(45);
+    double j=tan(45);
+    printf("\n%lf",a);
+    printf("\n%lf",b);
+    printf("\n%d",c);
+    printf("\n%d",d);
+    printf("\n%d",e);
+    printf("\n%lf",f);
+    printf("\n%lf",g);
+    printf("\n%lf",h);
+    printf("\n%lf",i);
+    printf("\n%lf",j);
+    return 0;
+}

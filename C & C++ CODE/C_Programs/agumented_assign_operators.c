4ababa85d6c6025198b446d823be636c06db2529
Rohitpotdar44 <potdarrohit59@gmail.com>
2023-09-27 16:24:46 +0530

    C Programs

---
+++ C & C++ CODE/C_Programs/agumented_assign_operators.c
@@ -0,0 +1,14 @@
+//agumented assignment operators = ex. x=x+5 so used x+=5 instead 
+#include<stdio.h>
+int main()
+{
+    int x=10;
+    x+=5;
+    printf("%d\n",x);
+    x-=3;
+    printf("%d\n",x);
+    x*=6;
+    printf("%d\n",x);
+    return 0;
+
+} 

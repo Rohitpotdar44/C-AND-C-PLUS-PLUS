3c2e71a5f4a5793fef546ebce3a374b59fa31b9a
Rohitpotdar44 <potdarrohit59@gmail.com>
2023-09-27 15:42:53 +0530

    C programs

---
+++ C & C++ CODE/C_Programs/constants.c
@@ -0,0 +1,8 @@
+//constants= fixed value that cannot be changed during program execution
+#include<stdio.h>
+int main()
+{
+    const float PI=3.14159;
+    printf("Value of PI is:%f",PI);
+    return 0;
+}
